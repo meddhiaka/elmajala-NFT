b8f895bfbd6c31e1ab89ae1c1d274ff9de13bfdd
Med Dhia Kassab <mohameddhia.kassab@fsb.ucar.tn>
2024-04-05 04:29:29 +0100

    code: landing page active

--- elmajala-ui/app/page.tsx
+++ elmajala-ui/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import { AuroraBackgroundDemo } from "@/components/LandingBackground";
 
 export default function Home() {
   return (
     <div>
-      hello
+      <AuroraBackgroundDemo />
     </div>
   );
 }

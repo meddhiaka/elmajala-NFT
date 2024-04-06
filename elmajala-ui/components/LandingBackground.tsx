b8943a9fd211e78c8df7a8de101459cb9998dd4b
Med Dhia Kassab <mohameddhia.kassab@fsb.ucar.tn>
2024-04-06 04:05:16 +0100

    code: responsive landing headings fixed

--- elmajala-ui/components/LandingBackground.tsx
+++ elmajala-ui/components/LandingBackground.tsx
@@ -19,11 +19,11 @@ export function AuroraBackgroundDemo() {
                 }}
                 className="relative flex flex-col gap-4 items-center justify-center px-4"
             >
-                <div className="my-1 text-5xl sm:text-6xl md:text-7xl lg:text-9xl font-black text-transparent bg-clip-text bg-gradient-to-r from-red-400 to-white ">elmajala-NFT</div>
-                <div className="text-xl md:text-3xl font-bold dark:text-white text-center">
+                <div className="my-1 text-[3.5rem] sm:text-7xl md:text-8xl lg:text-9xl font-black text-transparent bg-clip-text bg-gradient-to-r from-red-400 to-white ">elmajala-NFT</div>
+                <div className="text-[0.8rem] sm:text-[1rem] md:text-[1.4rem] lg:text-3xl font-bold dark:text-white text-center">
                     représente une initiative créative d'origine <span className="text-red-400">Tunisienne</span> <span className=" inline-block"><Image src={"/tunisia.png"} alt="" width={30} height={30} /></span> .
                 </div>
-                <div className="font-extralight text-md text-justify dark:text-neutral-200 py-2 max-w-sm sm:max-w-xl">
+                <div className="font-extralight text-xs sm:text-sm md:text-md text-justify dark:text-neutral-200 py-2 max-w-sm sm:max-w-xl">
                     Produire de magnifiques cartes NFT mettant en valeur des sites tunisiens et explorer de nouvelles destinations à travers tout le pays, tout en découvrant la richesse culturelle de chaque région, pour promouvoir la diversité et l'authenticité de la Tunisie.
                 </div>
                 <ConnectButton />

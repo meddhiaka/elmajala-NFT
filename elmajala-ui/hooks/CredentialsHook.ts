5e906aef7adbf335d080d0671a4aed77035ba39f
Med Dhia Kassab <mohameddhia.kassab@fsb.ucar.tn>
2024-04-10 20:54:05 +0100

    code: implemented custom hook for demo auth

--- elmajala-ui/hooks/CredentialsHook.ts
+++ elmajala-ui/hooks/CredentialsHook.ts
@@ -1,14 +1,35 @@
 "use client"
 
+import { useRouter } from "next/navigation"
 import { useEffect } from "react"
 
+
 export default function InitCredentials() {
-    useEffect(()=> {
-        if (localStorage.getItem("permission") === null) {
-            localStorage.setItem("permission", "false"),
-            localStorage.setItem("address", "")
-            localStorage.setItem("balance", "")
-            console.log("init ok")
-          }
+    useEffect(() => {
+        initOrLogout(1)
     }, [])
 }
+
+export function initOrLogout(choice: number) {
+    if (choice == 1) {
+        // init feature (first time)
+        if ((localStorage.getItem("permission") === null) && (typeof window !== 'undefined')) {
+            if (typeof window !== 'undefined') {
+                localStorage.setItem("permission", "false")
+                localStorage.setItem("address", "")
+                localStorage.setItem("balance", "")
+                console.log("init ok")
+            }
+        }
+    } else if (choice == 2) {
+        // logout feature
+        if ((localStorage.getItem("permission") === "true") && (typeof window !== 'undefined')) {
+            if (typeof window !== 'undefined') {
+                localStorage.setItem("permission", "false")
+                localStorage.setItem("address", "")
+                localStorage.setItem("balance", "")
+                console.log("logout ok")
+            }
+        }
+    }
+} 

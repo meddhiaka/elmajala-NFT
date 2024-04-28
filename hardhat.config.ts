02e09cf9e0d944f3004731295a027896806047c9
Med Dhia Kassab <mohameddhia.kassab@fsb.ucar.tn>
2024-04-28 17:31:54 +0100

    code: deployment configuration to Sepolia testnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,19 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+
+dotenv.config()
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: process.env.NODE_API_URL,
+      accounts: [`0x${process.env.NODE_PRIVATE_KEY}`]
+    }
+  }
 };
 
 export default config;
